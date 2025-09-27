f3b711947bfd4e129d74504ce7a1353bf46b0ed4
shanuz <shanupm6181@gmail.com>
2025-09-27 16:24:47 +0000

    seeded but no product shows

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,6 +1,8 @@
+'use client';
+
 import { initializeApp, getApps, type FirebaseApp } from "firebase/app";
-import { getAuth, connectAuthEmulator, type Auth } from "firebase/auth";
-import { getFirestore, connectFirestoreEmulator, type Firestore } from "firebase/firestore";
+import { getAuth, type Auth } from "firebase/auth";
+import { getFirestore, type Firestore } from "firebase/firestore";
 import { firebaseConfig } from "./config";
 
 function initializeFirebase(): { app: FirebaseApp; auth: Auth; firestore: Firestore } {
@@ -8,22 +10,6 @@ function initializeFirebase(): { app: FirebaseApp; auth: Auth; firestore: Firest
   const app = apps.length > 0 ? apps[0] : initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const firestore = getFirestore(app);
-
-  if (process.env.NEXT_PUBLIC_EMULATOR_HOST) {
-    const host = process.env.NEXT_PUBLIC_EMULATOR_HOST;
-    // Important: The connectXXXEmulator functions below will fail if the emulators
-    // are not running. Make sure you have started the emulators with `firebase emulators:start`
-    // before running the app.
-    try {
-      connectAuthEmulator(auth, `http://${host}:9099`, { disableWarnings: true });
-      connectFirestoreEmulator(firestore, host, 8080);
-    } catch (e) {
-      console.error(
-        "Could not connect to Firebase emulators. Make sure they are running."
-      );
-      console.error(e);
-    }
-  }
   
   return { app, auth, firestore };
 }
